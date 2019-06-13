02d6446c61f7787d068a5c9cdb161211958fa853
EC2 Default User <ec2-user@ip-172-31-42-116.us-east-2.compute.internal>
2019-06-13 04:09:03 +0000

    Adding Everything

--- application.py
+++ application.py
@@ -11,7 +11,6 @@ from flask import Flask, render_template, request
 from application import db
 from application.models import Data
 from application.forms import EnterDBInfo, RetrieveDBInfo
-
 # Elastic Beanstalk initalization
 application = Flask(__name__)
 application.debug=True
--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # edit the URI below to add your RDS password and your AWS URL
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://timleo:sucksomeDicks0-@timtime.ccqv3rhgwzsj.us-east-2.rds.amazonaws.com/timleo'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
