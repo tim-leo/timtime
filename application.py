3628e69f199173a5d477704208b2fefa1a840f96
tim <timothy.leo.robertson@gmail.com>
2019-06-13 06:01:54 +0000

    added the html and css

--- application.py
+++ application.py
@@ -1,10 +1,5 @@
 '''
-Simple Flask application to test deployment to Amazon Web Services
-Uses Elastic Beanstalk and RDS
-
-Author: Scott Rodkey - rodkeyscott@gmail.com
-
-Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
+Created by Tim Robertson (mostly. I've googled a lot of stuff)
 '''
 
 from flask import Flask, render_template, request
@@ -47,4 +42,4 @@ def index():
     return render_template('index.html', form1=form1, form2=form2)
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=8080)
