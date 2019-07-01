8377e0a5df8df8a41d4138bb8318ab019041710b
tim <timothy.leo.robertson@gmail.com>
2019-07-01 04:18:04 +0000

    fixed the image

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://timleo:sucksomeDicks0-@timtime.ccqv3rhgwzsj.us-east-2.rds.amazonaws.com/timleo'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://timleo:sucksomeDicks0-@timtime.ccqv3rhgwzsj.us-east-2.rds.amazonaws.com/timleo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://timleo:sucksomeDicks0-@aa1fvq8qseo7j9m.ccqv3rhgwzsj.us-east-2.rds.amazonaws.com:3306/aa1fvq8qseo7j9m'
+
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
